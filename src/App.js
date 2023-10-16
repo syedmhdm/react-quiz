b517c4d9c5269fe6be58ad90bb5ca22b1973215a
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-10-17 02:34:09 +0530

    changed useState hook with useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className='App'></div>;
-}
+import DateCounter from "./DateCounter";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
